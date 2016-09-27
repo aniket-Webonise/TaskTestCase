ff60b32df50c3476fa94d8600a3f2e83873487f6
aniket-webonise <aniket.kumar@weboniselab.com>
2016-09-27 15:02:04 +0530

    code with test case for model and presenter use of recyclerview and co-ordinator layout,cardview for layout

--- app/src/main/java/com/example/webonise/tasktestcase/model/Comments.java
+++ app/src/main/java/com/example/webonise/tasktestcase/model/Comments.java
@@ -1,5 +1,9 @@
 package com.example.webonise.tasktestcase.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
 /**
  * Created by webonise on 22/9/16.
  */
@@ -8,6 +12,21 @@ public class Comments {
     public String name;
     public String email;
     public String body;
+
+
+    @SerializedName("comments")
+    private ArrayList<Comments> commentsArrayList;
+
+
+    public ArrayList<Comments> getCommentsArrayList() {
+        return commentsArrayList;
+    }
+
+    public void setManagers(ArrayList<Comments> commentsArrayList) {
+        this.commentsArrayList = commentsArrayList;
+    }
+
+
 
     public String getLengthIncreases() {
         return lengthIncreases;
